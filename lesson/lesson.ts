d61d77f12d50be64948b4a4508cabb9adeec473e
HP_Nam <namphg1412@gmail.com>
2025-05-03 22:30:38 +0700

    Tuple types

--- lesson/lesson.ts
+++ lesson/lesson.ts
@@ -1,10 +1,20 @@
-// Array Type
+// Tuple types
 
-//mixed types: khai báo nhiều type cho 1 biến
-let test: (string | number)[] = ["nam"];
+let skills = ['HTML', 31];
+skills.push('CSS'); // OK
+skills.push(55); // OK
 
-test.push("hoang");
+// tuple: dataType, order, size
+let skills2: [string, number] = ['Javascript', 38];
 
-test.push(18);
+let skills3: [boolean, string, number];
+skills3 = [true, 'HTML', 25]; // OK
 
-console.log(">>> check array ", test);
+/**
+ * - Số phần tử của tuple cần được khai báo trước (cố định)
+ * - Type của từng phần tử trong array cần được khai báo trước, và không nhất thiết phải giống nhau
+ * - Với tuple, thứ tự của phần tử rất QUAN TRỌNG
+ */
+
+// Optional tuple elements, bắt buộc đặt ở cuối
+let skills4: [boolean, string, number?] = [true, 'hello']; // OK
