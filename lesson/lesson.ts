f2278934d8e38f1974b57f5a27cbe72362a0507e
HP_Nam <namphg1412@gmail.com>
2025-05-04 13:56:38 +0700

    Any Type

--- lesson/lesson.ts
+++ lesson/lesson.ts
@@ -1,23 +1,7 @@
-// Enum types
+// Any Type
 
-// Enum (enumerated : liệt kê, one by one) là 1 nhóm các giá trị hằng số.
+let namev2: any = 'Nam';
 
-// enum API_STATUS {
-//     PENDING, FULFILLED, REJECTED
-// }
+namev2 = true; //ok
 
-// let a1 = API_STATUS.PENDING; //0
-// let a2 = API_STATUS.FULFILLED; //1
-
-// console.log(">>> check a1 = ", a1, " a2 = ", a2);
-
-enum API_STATUS {
-    PENDING = "PENDING",
-    FULFILLED = "FULFILLED",
-    REJECTED = "REJECTED"
-}
-
-let a1 = API_STATUS.PENDING; //PENDING
-let a2 = API_STATUS.FULFILLED; //FULFILLED
-
-console.log(">>> check a1 = ", a1, " a2 = ", a2);
+namev2 = 98; //ok
