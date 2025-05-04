add0722a687b4f80f85d2e4088120e64afe91483
HP_Nam <namphg1412@gmail.com>
2025-05-04 14:06:25 +0700

    Void Type

--- lesson/lesson.ts
+++ lesson/lesson.ts
@@ -1,7 +1,13 @@
-// Any Type
+// Void Type
 
-let namev2: any = 'Nam';
+// bình thường hàm sẽ trả về giá trị nào đó, nhưng có những hàm không cần trả về giá trị nào cả
+const sum = (a: number, b: number): number => {
+    return a + b;
+}
 
-namev2 = true; //ok
-
-namev2 = 98; //ok
+// ví dụ như hàm console.log, hàm này không cần trả về giá trị nào cả, chỉ cần thực hiện hành động là được
+// void => ko trả về giá trị nào cả
+// không ghi void cũng được, nhưng ghi vào sẽ giúp code tường minh hơn
+const handleLog = (message: string): void => {
+    console.log(">>> message: ", message);
+}
