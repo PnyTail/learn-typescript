d139c986d31c22e759e08c9bd3720bce5e9baed4
HP_Nam <namphg1412@gmail.com>
2025-05-04 22:29:39 +0700

    Function types

--- lesson/lesson.ts
+++ lesson/lesson.ts
@@ -1,6 +1,7 @@
-// Functions
+// Function types
 
-const sum = (a: number, b: number) => {
+// gán type cho giá trị trả về của hàm, tuy nhiên không nhất thiết phải làm như vậy
+const sum = (a: number, b: number): number => {
     return a + b;
 }
 
