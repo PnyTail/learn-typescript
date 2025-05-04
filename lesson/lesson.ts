639dcf5812b8feb9ef5b858aaeb76179035a9215
HP_Nam <namphg1412@gmail.com>
2025-05-04 14:25:00 +0700

    Never Type

--- lesson/lesson.ts
+++ lesson/lesson.ts
@@ -1,13 +1,17 @@
-// Void Type
+// Never Type
 
-// bình thường hàm sẽ trả về giá trị nào đó, nhưng có những hàm không cần trả về giá trị nào cả
-const sum = (a: number, b: number): number => {
-    return a + b;
+const handleException = (errorMessage: string): never => {
+    throw Error(errorMessage);
 }
 
-// ví dụ như hàm console.log, hàm này không cần trả về giá trị nào cả, chỉ cần thực hiện hành động là được
-// void => ko trả về giá trị nào cả
-// không ghi void cũng được, nhưng ghi vào sẽ giúp code tường minh hơn
-const handleLog = (message: string): void => {
-    console.log(">>> message: ", message);
+// handleException("This is an error message");
+
+// void trả về undefined
+// never không trả về gì cả
+
+const test = (): void => {
+
 }
+
+let a = test(); // undefined
+console.log(">> check a : ", a); // undefined
