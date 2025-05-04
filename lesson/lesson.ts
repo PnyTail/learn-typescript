faaa99bcb72acb426426d2234bf927a78b3c7289
HP_Nam <namphg1412@gmail.com>
2025-05-04 15:05:27 +0700

    If else

--- lesson/lesson.ts
+++ lesson/lesson.ts
@@ -1,17 +1,10 @@
-// Type Aliases
+// If else
 
-type namHoangType = number | string | boolean | object;
-
-function addNumberOrString(a: namHoangType, b: number | string) {
-    if (typeof a === 'number' && typeof b === 'number') {
-        return a + b;
-    }
-    if (typeof a === 'string' && typeof b === 'string') {
-        return a.concat(b);
-    }
-    throw new Error('Parameters must be numbers or strings');
+let age: number = 12;
+if (age > 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
 }
 
-// lỗi khi run code
-// lỗi khi compile code
-console.log(">>> check : ", addNumberOrString('hahhie', ' bựa'));
+// else if() {}
