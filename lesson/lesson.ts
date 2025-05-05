5e21400b4a26141b59cf4378345e161589c79b20
HP_Nam <namphg1412@gmail.com>
2025-05-06 01:02:02 +0700

    readonly

--- lesson/lesson.ts
+++ lesson/lesson.ts
@@ -1,17 +1,14 @@
-// Access Modifiers
+// readonly
 
-class Employee {
-    public empCode: string;
-    private empName: string;
-    //protected empAddress: string; // protected: only accessible within class and subclasses
+class Person {
+    readonly birthDate: Date; // Readonly property
+    // The readonly modifier makes the property immutable after the constructor has been called.
+    // This means that the property can only be assigned a value in the constructor and cannot be changed afterwards.
+    // This is useful for properties that should not be changed after the object is created, such as a birth date.
 
-    constructor(empCode: string, empName: string) {
-        this.empCode = empCode;
-        this.empName = empName;
+    constructor(birthDate: Date) {
+        this.birthDate = birthDate;
     }
 }
-let emp = new Employee("adsfg", "egfb");
-emp.empCode = "123";
-// emp.empName = "Nam Hoang"; //error: Property 'empName' is private and only accessible within class 'Employee'.
-
-console.log(">>> check emp: ", emp);
+let person = new Person(new Date(1990, 12, 25));
+// person.birthDate = new Date(1991, 12, 25); // Compile error
