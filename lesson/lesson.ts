b411ff6d130cf729e625a76f544840a1845b19a0
HP_Nam <namphg1412@gmail.com>
2025-05-06 16:45:08 +0700

    Abstract Classes

--- lesson/lesson.ts
+++ lesson/lesson.ts
@@ -1,13 +1,43 @@
-// Static Methods and Properties
+// Abstract Classes
 
-class Circle {
-    static pi: number = 3.14; // Static property
-    public test: number = 69;
+abstract class Employee {
+    constructor(private firstName: string, private lastName: string) { }
 
-    static calculateArea(radius: number): number { // Static method
-        return this.pi * radius * radius;
+    abstract getSalary(): number; // Abstract method
+
+    // normal method
+    get FullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    compensationStatement(): string {
+        return `${this.FullName} makes ${this.getSalary()} a month.`;
     }
 }
 
-// console.log(">>> check pi = ", Circle.pi); // Accessing static property
-console.log(">>> check area = ", Circle.calculateArea(10)); // Accessing static method
+// không thể tạo 1 object từ abstract class, chỉ có thể tạo ra các class con kế thừa từ nó
+// => dùng kế thừa để sử dụng abstract class
+
+class FullTimeEmployee extends Employee {
+    constructor(firstName: string, lastName: string, private Salary: number) {
+        super(firstName, lastName); // dùng super để kế thừa lại cha
+    }
+
+    // cần viết lại method này, vì nó được khai báo trong abstract class ở trên chứ chưa được đinh nghĩa
+    getSalary(): number {
+        return this.Salary;
+    }
+}
+
+class Contractor extends Employee {
+    constructor(firstName: string, lastName: string, private rate: number, private hours: number) {
+        super(firstName, lastName); // dùng super để kế thừa lại cha
+    }
+
+    getSalary(): number {
+        return this.rate * this.hours;
+    }
+}
+
+const test1 = new FullTimeEmployee("Nam", "Nguyen", 1000);
+console.log(">>> check test1: ", test1.getSalary());
