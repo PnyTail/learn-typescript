3906602e4e1c9cd94da2ac36de675daa36c1f2a8
HP_Nam <namphg1412@gmail.com>
2025-12-05 11:55:26 +0700

    khai báo mảng (Array)

---
+++ src/learn/chap10/vid89/vid89.ts
@@ -0,0 +1,13 @@
+export { };
+
+const name: string[] = ["Nam", " Hoang"];
+
+const scores: number[] = [1, 2, 3];
+
+//option 2: generic
+const name2: Array<string> = ["Nam", " Hoang"];
+
+const scores2: Array<number> = [1, 2, 3];
+
+console.log("name: ", name);
+console.log("scores: ", scores);
