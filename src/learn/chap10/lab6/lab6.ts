7963e0897ac5fdba4b3b6cd33c6944deb1360dc7
HP_Nam <namphg1412@gmail.com>
2025-12-09 11:46:56 +0700

    lab6

---
+++ src/learn/chap10/lab6/lab6.ts
@@ -0,0 +1,76 @@
+export { };
+//lab 6
+
+type TStudent = {
+    id: number;
+    name: string;
+    email: string;
+    isPremium: boolean;
+    contact: string | number;
+}
+
+type TCourse = {
+    courseId: number;
+    title: string;
+    price: number;
+    students: TStudent[];
+}
+
+const registerStudentToCourse = (student: TStudent, course: TCourse) => {
+    console.log("Thêm học viên vào danh sách khoá học: ", course.title);
+    course.students.push(student);
+    console.log(`Thêm thành công học viên ${student.name} vào khoá học: ${course.title}`);
+}
+
+const printCourseInfo = (course: TCourse) => {
+    const quantityStudentInCourse = course.students.length;
+    console.log(`Tên khoá học: ${course.title}`);
+    if (quantityStudentInCourse > 0) {
+        console.log(`Số học viên đã đăng ký: ${quantityStudentInCourse}`);
+        course.students.forEach((student) => {
+            console.log(student.name)
+        })
+    } else {
+        console.log("Chưa có học viên nào!");
+    }
+}
+
+const main = () => {
+    const student1: TStudent = {
+        id: 12,
+        name: "Nguyễn Văn An",
+        email: "NVA@gmail.com",
+        isPremium: true,
+        contact: 901234567
+    }
+
+    const student2: TStudent = {
+        id: 15,
+        name: "Trần Thị Bích",
+        email: "TTB@gmail.com",
+        isPremium: false,
+        contact: 912345678
+    }
+
+    const learnJavaScript: TCourse = {
+        courseId: 21,
+        title: "Học JavaScript",
+        price: 248,
+        students: []
+    }
+
+    const learnTypeScript: TCourse = {
+        courseId: 212,
+        title: "Học TypeScript",
+        price: 2489,
+        students: []
+    }
+
+    registerStudentToCourse(student1, learnTypeScript);
+    console.log("=============")
+    printCourseInfo(learnJavaScript);
+    console.log("=============")
+    printCourseInfo(learnTypeScript);
+}
+
+main();
