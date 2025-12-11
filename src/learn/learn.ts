3ed0e10670667de55e121780faf5f16d06c2948c
HP_Nam <namphg1412@gmail.com>
2025-12-11 13:28:47 +0700

    literal & enum types

--- src/learn/learn.ts
+++ src/learn/learn.ts
@@ -1,15 +1,43 @@
 export { };
-// vid 99
+// vid 100
 
-//null & undefined
+// Literal types, Enum types
 
-type TName = string | null;
+type TRole = string | number;
 
-let name: TName = null;
+type TSuperRole = "USER" | "SUPER_ADMIN" | "ADMIN"; //literal types
+// type TSuperRole = "USER" | "SUPER_ADMIN" | "ADMIN" | string; // vấn đề của literal type là có thể bị sửa => thêm type khác vào
+//bị người khác thêm string vào type
 
-const printName = (myName: TName) => {
-    myName?.toUpperCase(); // optional chaining (?.)
-    (myName!).toLowerCase(); // non-null (!)
-    if (myName) myName.toUpperCase(); // hoặc đơn giản là check bằng if
-    console.log("name: ", myName);
+const user: TRole = "USER";
+const admin: TRole = "ADMIN";
+const administrator: TRole = 1;
+
+const otherUser: TSuperRole = "ADMIN";
+
+enum RoleEnum {
+    USER, //0
+    SUPER_ADMIN = 7, //1
+    ADMIN //2
 }
+
+//nếu gán số thì sau đó nó sẽ tự tăng dần lên
+
+enum RoleEnum2 {
+    USER = "USER",
+    SUPER_ADMIN = "SUPER_ADMIN",
+    ADMIN = "ADMIN"
+} //có thể khởi tạo giá trị cho enum
+
+const myRole1: RoleEnum = RoleEnum.USER;
+const myRole2: RoleEnum = RoleEnum.ADMIN;
+const myRole3: RoleEnum = RoleEnum.SUPER_ADMIN;
+console.log(">> check enum myRole1: ", myRole1); //0
+console.log(">> check enum myRole2: ", myRole2); //2
+console.log(">> check enum myRole3: ", myRole3);
+console.log("===================");
+
+const role1: RoleEnum2 = RoleEnum2.SUPER_ADMIN;
+const role2: RoleEnum2 = RoleEnum2.ADMIN;
+console.log(">>> check role1 enum 2: ", role1);
+console.log(">>> check role2 enum 2: ", role2);
