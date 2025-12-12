9b312846cc9f33dbf6d1ee4ebd83db6e297cc064
HP_Nam <namphg1412@gmail.com>
2025-12-12 23:46:45 +0700

    106 - Type Assertions (ép kiểu type)

--- src/learn/chap11/vid105.ts
+++ src/learn/chap11/vid105.ts
@@ -1,4 +1,10 @@
 export { };
-console.log("vid 105");
-//vid 105
+console.log("vid 106");
+//vid 106
 
+const BtnElement = document.getElementById("myBtn");
+const inputElement = document.getElementById("name") as HTMLInputElement; //ép kiểu dùng từ "as", hoặc dùng generic <>
+
+BtnElement?.addEventListener("click", () => {
+    alert(inputElement!.value);
+})
