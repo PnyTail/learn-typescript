71d5473f0dbe0d2b33e3e696aa93bddc17ce7683
HP_Nam <namphg1412@gmail.com>
2025-12-13 15:17:52 +0700

    improve code quality (lab7)

--- src/learn/chap11/lab7/lab7.ts
+++ src/learn/chap11/lab7/lab7.ts
@@ -2,34 +2,32 @@ export { };
 console.log("lab 7");
 console.log("===================");
 
-interface IBlogs {
+interface IBlog {
     id: number;
     title: string;
     author: string;
     content: string;
 }
 
-const tableElement = document.getElementById("blogs");
-const tbody = document.querySelector("#blogs tbody");
-
 const fetchBlogs = async () => {
     const res = await fetch("http://localhost:8000/blogs");
-    const data = await res.json() as IBlogs[];
+    const data = await res.json() as IBlog[];
+    const tbody = document.querySelector("#blogs tbody");
+
+    // check lại hình thù data (dùng thư viện: joi, zod, ...)
 
     if (data && data.length) {
         data.forEach((blog) => {
             tbody!.innerHTML += `
-            <tr>
-                <td>${blog.id}</td>
-                <td>${blog.title}</td>
-                <td>${blog.author}</td>
-                <td>${blog.content}</td>
-            </tr>
+                <tr>
+                    <td>${blog.id}</td>
+                    <td>${blog.title}</td>
+                    <td>${blog.author}</td>
+                    <td>${blog.content}</td>
+                </tr>
             `
         })
     }
-
-    // console.log("check table: ", data);
 }
 
 fetchBlogs();
