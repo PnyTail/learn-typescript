72abd11e7e2a924be43a8e53ea85d3a26242bb69
HP_Nam <namphg1412@gmail.com>
2025-12-16 16:08:05 +0700

    115 - Access Modifiers – Giới hạn truy cập

--- src/learn/chap12/chap12.ts
+++ src/learn/chap12/chap12.ts
@@ -1,23 +1,22 @@
 export { };
-console.log("vid 114");
+console.log("vid 115");
 
-class Person {
-    name: string;
-    age: number;
+class Animal {
+    //fields
+    public name: string;
+    private id: number;
 
-    // hàm tạo
-    constructor();
-    constructor(name: string, age: number);
+    constructor(name: string, id: number) {
+        this.name = name;
+        this.id = id;
+    }
 
-    constructor(name?: string, age?: number) {
-        this.name = name ?? "unknown name";
-        this.age = age ?? 1;
+    //method
+    makeSound() {
+        console.log("é é é with id: ", this.id);
     }
 }
 
-const person1 = new Person("Nam-Hoang", 19);
-console.log(`my name is: ${person1.name} and age: ${person1.age}`);
-
-console.log("=================");
-const person2 = new Person();
-console.log(`my name 2 is: ${person2.name} and age 2: ${person2.age}`);
+const myDog = new Animal("dog1", 151);
+console.log(`animal name: ${myDog.name}`);
+myDog.makeSound();
