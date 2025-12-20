281183c073a909bed6a5ead79f5dabfe82d7c093
HP_Nam <namphg1412@gmail.com>
2025-12-20 13:11:47 +0700

    130 - generics constraints

--- src/learn/chap13/chap13.ts
+++ src/learn/chap13/chap13.ts
@@ -1,41 +1,71 @@
 export { };
-console.log("vid 129");
+console.log("vid 130");
 
-interface IDataString {
-    data: string;
+// function logLength1<T>(value: T) {
+//     console.log(value.length); //không chắc kiểu T có thuộc tính length
+// }
+
+// ràng buộc. kiểu T phải có thuộc tính length
+function logLength<T extends { length: number }>(value: T) {
+    console.log(value.length);
 }
 
-interface IDataNumber {
-    data: number;
-}
+logLength("xinChao");
+logLength([1, 2, 4]); //cả 2 phía trên đúng vì có thuộc tính length
 
-interface IData<T> {
-    data: T;
-}
+// logLength(1234); // lỗi, vì number không có thuộc tính length
 
-const a1: IData<string> = { data: "tha thu" };
-
-const a2: IData<number> = { data: 541 };
-
-interface IAPIResponse<T> {
-    status: number;
-    data: T
-}
-
+//ràng buộc interface
 interface IUser {
     id: number;
     name: string;
-    email: string
 }
 
-const fetchUsers = async () => {
-    const res = await fetch("http://localhost:8000/users");
-    const dataAPI = await res.json() as IUser[];
+function testInterface<T extends IUser>(value: T) {
+    console.log(value)
+}
 
-    const result: IAPIResponse<IUser[]> = {
-        status: 200,
-        data: dataAPI
+testInterface({ id: 12, name: "hello" }); //ok
+testInterface({ id: 12, name: "hello", age: 41 }); //không lỗi vì có đủ, hơn cũng không sao => miễn là thoả mãn TỐI THIỂU
+// testInterface({ id: 123 }); //lỗi vì thiếu thuộc tính name => ràng buộc
+
+
+// ràng buộc class
+class Animal {
+    move() {
+        console.log("moving...");
     }
-
-    result.data.map(item => item.email);
 }
+
+class Dog extends Animal {
+    bark() {
+        console.log("wolf!");
+    }
+}
+
+function testClass<T extends Animal>(value: T) {
+    console.log(value.move());
+
+    if (value instanceof Dog) {
+        value.bark();
+    }
+}
+
+const a = new Animal();
+const b = new Dog();
+
+testClass(a);
+testClass(b);
+
+//ràng buộc keyof
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key];
+}
+
+const user = {
+    name: "Kirito",
+    age: 18
+}
+
+getProperty(user, "age"); //tham số thứ 2 - phải là key của tham số thứ 1 (ở đây là object user)
+// getProperty(user, "email"); //lỗi
