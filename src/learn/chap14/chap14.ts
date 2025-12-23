215ba940420de5de2bc28760f9f3fe72439461b1
HP_Nam <namphg1412@gmail.com>
2025-12-23 11:47:37 +0700

    134 - create, export/import a module

---
+++ src/learn/chap14/chap14.ts
@@ -0,0 +1,9 @@
+
+// export, import => module
+const bien1 = "bla bla";
+
+const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+export { sum, bien1 }
---
+++ src/learn/chap14/test.ts
@@ -0,0 +1,4 @@
+import { sum, bien1 } from "./chap14";
+
+const test = bien1;
+console.log(sum(6, 9));
