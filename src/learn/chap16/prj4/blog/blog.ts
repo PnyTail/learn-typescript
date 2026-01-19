14676574feb4cda38f8bd033418fee753f58f6b8
HP_Nam <namphg1412@gmail.com>
2026-01-19 11:39:18 +0700

    display blog list (prj4 - p1)

--- src/learn/chap16/prj4/blog/blog.ts
+++ src/learn/chap16/prj4/blog/blog.ts
@@ -1,2 +1,35 @@
+export { };
+interface IBlog {
+    id: number;
+    title: string;
+    author: string;
+    content: string;
+}
+
+const fetchBlogs = async () => {
+    const res = await fetch("http://localhost:8000/blogs");
+    const data = await res.json() as IBlog[];
+    const tbody = document.querySelector("#tableBlog tbody")!;
+
+    if (data && data.length) {
+        data.forEach((blog, index) => {
+            tbody.innerHTML += `
+                <tr>
+                    <th scope="row">${index + 1}</th>
+                    <td>${blog.id}</td>
+                    <td>${blog.title}</td>
+                    <td>${blog.author}</td>
+                    <td>${blog.content}</td>
+                    <td>
+                        <button class="btn btn-warning">Edit</button>
+                        <button class="btn btn-danger">Delete</button>
+                    </td>
+                </tr>
+            `
+        })
+    }
+}
+
+fetchBlogs();
 
 console.log("blog.ts")
